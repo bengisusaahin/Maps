6edadaf6225c2a7f3b7b474e9923fa8c7f3ba7e2
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2023-10-20 20:14:23 +0300

    Flowable

--- app/src/main/java/com/example/maps/view/MainActivity.java
+++ app/src/main/java/com/example/maps/view/MainActivity.java
@@ -2,23 +2,57 @@ package com.example.maps.view;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.room.Room;
 
 import android.content.Intent;
+import android.net.LinkAddress;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.example.maps.R;
+import com.example.maps.adapter.PlaceAdapter;
+import com.example.maps.databinding.ActivityMainBinding;
+import com.example.maps.model.Place;
+import com.example.maps.roomdb.PlaceDao;
+import com.example.maps.roomdb.PlaceDatabase;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+    PlaceDatabase db;
+    PlaceDao placeDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        db = Room.databaseBuilder(getApplicationContext(),PlaceDatabase.class,"Places")
+                //.allowMainThreadQueries()
+                .build();
+        placeDao = db.placeDao();
 
+        compositeDisposable.add(placeDao.getAll()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(MainActivity.this::handleResponse)
+        );
+    }
+
+    private void handleResponse(List<Place> placeList){
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        PlaceAdapter placeAdapter = new PlaceAdapter(placeList);
+        binding.recyclerView.setAdapter(placeAdapter);
     }
 
     @Override
@@ -35,5 +69,11 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.clear();
     }
 }
